627cd405e46f00159c8231af153af46ffd5a6491
snacksquid <spilliams@gmail.com>
2023-12-30 22:25:30 -0600

    Add tailwind and prettier integration

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="text-xl font-bold flex item-left">
+      <image
+        class="h-24 w-24"
+        src="/squid.png"
+        alt="a logo of a squid drawn in pixel art style"
+      />
+      <h1>snacksquid</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,9 @@
+function MainPage() {
+  return (
+    <div>
+      <h1>Welcome!</h1>
+      <p>Hello world! This is my webpage</p>
+    </div>
+  );
+}
+export default MainPage;
